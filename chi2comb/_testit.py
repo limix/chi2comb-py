338df3a9ecb76b27264ba9c8c14bd953d70ef1ba
Danilo Horta <danilo.horta@gmail.com>
2019-05-10 18:30:50 +0100

    ignore libpath.py

--- chi2comb/_testit.py
+++ chi2comb/_testit.py
@@ -12,7 +12,7 @@ def test(verbose=True):
         Exit code: ``0`` for success.
     """
 
-    args = ["--doctest-modules", "--doctest-plus"]
+    args = ["--doctest-modules", "--doctest-plus", "--ignore='libpath.py'"]
     if not verbose:
         args += ["--quiet"]
 
